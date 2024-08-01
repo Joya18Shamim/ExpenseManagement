321b6830ff9181b967cf697b9a8e74cbb2c43472
Joya18Shamim <shamimjoya10@gmail.com>
2024-08-01 17:38:50 +0530

    Updating

---
+++ routes/transectionRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const {
+  addTransection,
+  getAllTransection,
+  editTransection,
+  deleteTransection,
+} = require("../controllers/transectionCtrl");
+
+// router object
+const router = express.Router();
+
+// routers
+
+// add transection POST method
+router.post("/add-transection", addTransection);
+
+// edit transection POST method
+router.post("/edit-transection", editTransection);
+
+// delete transection POST method
+router.post("/delete-transection", deleteTransection);
+
+// get transection
+router.post("/get-transection", getAllTransection);
+
+module.exports = router;
